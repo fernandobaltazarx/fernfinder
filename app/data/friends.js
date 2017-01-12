16f2652068bd04ad6d3e23c4a5aa9bf7f0ce2b1f
Fernando <fernandobaltazarx@gmail.com>
2017-01-11 18:04:38 -0800

    working verion

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendList = [
+  {
+    name: 'Fernando Baltazar',
+    profilePic: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/12118799_10153693605248832_8332150846494183633_n.jpg',
+    scores: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+
+]
+
+module.exports = friendList;
